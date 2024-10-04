2068de7ed823ac2932b79f248bfd936e05b85b0e
Nowshen Anjuman Khan Prapte <nowshen.anjum@gmail.com>
2024-10-04 21:41:00 +0600

    update

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://nowshen:nowshen@cluster0.pb86a.mongodb.net/', {
+    await mongoose.connect('localhost:27017/studentdb', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
